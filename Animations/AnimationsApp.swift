d76ddc9cf831d00d50c2a8678e26b12a4b8f7de9
LogicAndLines <betulzeynepseyis@gmail.com>
2025-07-28 15:31:51 +0200

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Zeynep Turnalı on 28.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
